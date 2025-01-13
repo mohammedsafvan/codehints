5fafc7c0360ec4473f335c0def65b537935a9a17
Mohammed Safvan <mohammedsafvannk@gmail.com>
2025-01-13 22:05:20 +0530

    login frontend and landing done

---
+++ frontend/app/login/page.jsx
@@ -0,0 +1,44 @@
+"use client";
+
+import { BackgroundGradient } from "@/components/ui/background-gradient";
+import { Input } from "@/components/ui/input";
+import { Button } from "@/components/ui/button";
+import { useState } from "react";
+
+const page = () => {
+  const [formData, setFormData] = useState({ email: "", password: "" });
+  return (
+    <div className="flex justify-center items-center h-screen bg-gray-950 dark ">
+      <div>
+        <BackgroundGradient className="rounded-[22px] w-[25vw]  p-4 sm:p-10 bg-white dark:bg-zinc-900">
+          <div className="flex flex-col text-white">
+            <div className="justify-center items-center w-full flex flex-col ">
+              <h1 className="font-bold text-3xl my-4">CodeHints💡</h1>
+              <h1 className="font-bold  justify-center text-xl">Login</h1>
+            </div>
+            <div className="flex mt-8 flex-col gap-y-5">
+              <Input
+                type="email"
+                placeholder="Email"
+                onChange={(e) =>
+                  setFormData({ ...formData, email: e.target.value })
+                }
+              />
+              <Input
+                type="password"
+                placeholder="Password"
+                onChange={(e) =>
+                  setFormData({ ...formData, password: e.target.value })
+                }
+              />
+              <Button>🔑 Login with Email</Button>
+              <Button> 🆕 Already have and account</Button>
+            </div>
+          </div>
+        </BackgroundGradient>
+      </div>
+    </div>
+  );
+};
+
+export default page;
--- frontend/app/page.js
+++ frontend/app/page.js
@@ -3,24 +3,28 @@
 import React from "react";
 import { useRouter } from "next/navigation";
 import { BackgroundGradientAnimation } from "@/components/ui/background-gradient-animation";
-import { Button } from "@/components/ui/button";
 
 export default function BackgroundGradientAnimationDemo() {
   const router = useRouter();
 
-  const handleClick = () => {
-    router.push("/problems");
-  };
-
   return (
     <BackgroundGradientAnimation>
       <div className="absolute z-50 inset-0 flex flex-col items-center justify-center text-white font-bold px-4 text-3xl text-center md:text-4xl lg:text-7xl">
-        <p className="bg-clip-text text-transparent drop-shadow-2xl bg-gradient-to-b from-white/80 to-white/20">
-          CodeHints⚡
-        </p>
-        <Button className="text-2xl m-4" size="lg" onClick={handleClick}>
-          Go to Problems
-        </Button>
+        <div className="flex">
+          <p className="bg-clip-text text-transparent drop-shadow-2xl bg-gradient-to-b from-white/80 to-white/20">
+            CodeHints
+          </p>
+          <span>💡</span>
+        </div>
+        <button
+          className="mt-4 relative inline-flex h-12 overflow-hidden rounded-full p-[1px] focus:outline-none focus:ring-2 focus:ring-slate-400 focus:ring-offset-2 transition-all focus:ring-offset-slate-50"
+          onClick={() => router.push("/login")}
+        >
+          <span className="absolute inset-[-1000%] animate-[spin_2s_linear_infinite] bg-[conic-gradient(from_90deg_at_50%_50%,#E2CBFF_0%,#393BB2_50%,#E2CBFF_100%)]" />
+          <span className="inline-flex text-xl h-full w-full cursor-pointer items-center justify-center font-semibold rounded-full bg-slate-950 px-7 hover:bg-slate-800 py-1 text-white backdrop-blur-3xl">
+            Get Started
+          </span>
+        </button>
       </div>
     </BackgroundGradientAnimation>
   );
